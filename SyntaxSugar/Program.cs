ff0dd9f7971b1f3e1f0a8abb6509def34cba08c4
A'Darius Craig <adariusc03@gmail.com>
2020-08-26 23:16:02 -0500

    Inferred,Interpolation,and Ternary

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inferred typing and Ternary operator
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
+
+
+
+            // String Interpolation
+
+            
+            var sum = 4;
+            string tell = (sum < 9) ? $"{sum} is greater than nine" : $" {sum} is less than nine";
+            Console.WriteLine(tell);
+
+
+        }
+    }
+}
